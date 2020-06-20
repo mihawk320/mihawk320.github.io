95c15d09030c189f255d4e3a3c1c598c51b60f0a
mihawk320 <carlosmora055@gmail.com>
2020-06-20 04:25:59 -0500

    actualizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,36 @@ function goepav(){
         imageAlt: 'Custom image',
       })
 }
+
+function serviteca(){
+    Swal.fire({
+        title: 'Serviteca',
+        text: 'Página realizada con HTML, CSS y JavaScript de un taller de mecánica, una simple muestra de diseño',
+        imageUrl: 'http://127.0.0.1:5501/assets/images/serviteca.png',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+}
+
+function hackaton(){
+    Swal.fire({
+        title: 'Hackaton',
+        text: 'Página realizada mientras estuve en una academia, la misión era construir una página en 4 horas con React, y este fue el resultado',
+        imageUrl: 'http://127.0.0.1:5501/assets/images/hackaton.png',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+}
+
+function adopta(){
+    Swal.fire({
+        title: 'AdoptaMed',
+        text: 'Página de adopción animal la cual está siendo construida en este momento, pero hay un adelanto. Es en React, SPA',
+        imageUrl: 'http://127.0.0.1:5501/assets/images/adoptamed.png',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+}
