412ddc058d7ef8deae8a7e08adbda98ca9639c93
Carlos Mora <carlosmora055@gmail.com>
2020-07-24 18:04:05 -0500

    actualizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,22 @@ function adopta(){
         imageAlt: 'Custom image',
       })
 }
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+  });
